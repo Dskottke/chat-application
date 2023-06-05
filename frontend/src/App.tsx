340acf32f5b55df4faf0b81eba7750ede6300274
davidskottke <david.skottke@neuefische.de>
2023-06-05 14:10:28 +0200

    Use Websocket frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import {useState} from 'react'
 import './App.css'
+import useChatWebSocket from "./useChatWebSocket";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [message, setMessage] = useState<string>()
+    const [messages, setMessages] = useState<string[]>([]);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    const receiveMessage = (message:string)=>{
+        console.log("Received message from backend", message)
+        setMessages([
+            ...messages,
+            message]);
+    }
+
+    const {send,connected} = useChatWebSocket(receiveMessage);
+
+
+  if(!connected) {
+      return <div>Connecting...</div>
+  }
+    return (
+        <div className="App">
+          {messages.map((message,index)=>{
+            return <div key={index}>{message}</div>
+          })}
+          <input onChange={(e)=>{setMessage(e.target.value)}}/>
+          <button onClick={()=>{send(message)}}>Send</button>
+        </div>
+    )
 }
 
 export default App
